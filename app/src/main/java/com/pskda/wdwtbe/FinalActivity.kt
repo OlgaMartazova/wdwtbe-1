91bc3e219e5a9c952ccf8e20c1ca0eb80bc22c36
OlgaMartazova <olyamarta34@gmail.com>
2021-07-03 16:09:08 +0300

    createActivities

---
+++ app/src/main/java/com/pskda/wdwtbe/FinalActivity.kt
@@ -0,0 +1,10 @@
+package com.pskda.wdwtbe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FinalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
